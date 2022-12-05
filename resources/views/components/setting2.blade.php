d91546bfc1b0421635d498b8d0e83f98e0101f57
unknown <jabrijuhinin93@gmail.com>
2022-12-05 14:48:44 +0800

    Decrease width panel for setting2.blade.php

--- resources/views/components/setting2.blade.php
+++ resources/views/components/setting2.blade.php
@@ -1,6 +1,6 @@
 @props(['heading'])
 
-<section class="max-w-5xl mx-auto mt-6">
+<section class="max-w-4xl mx-auto mt-6">
     <h1 class="text-lg font-bold mb-8 pb-2 border-b">{{ $heading }}</h1>
 
     <div class="flex">
