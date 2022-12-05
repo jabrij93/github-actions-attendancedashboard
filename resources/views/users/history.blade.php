bbc29e5482e56fa7572ef79ee4cc02b9cef50dcd
unknown <jabrijuhinin93@gmail.com>
2022-12-05 21:39:45 +0800

    Change history width size

--- resources/views/users/history.blade.php
+++ resources/views/users/history.blade.php
@@ -1,66 +1,62 @@
 <x-app-layout>
     <x-setting2 heading="Staff Attendance History">
         <div class="flex flex-col mt-6">
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 ">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <table class="min-w-0 mx-24 mb-14 divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    No.
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Staff ID
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Date Clock-in
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Time Clock-in
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Location Clock-in
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Time Clock-out
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Location Clock-out
-                                </th>
-                            </tr>
-                        </thead>
+            <table class="min-w-0 mx-4 mb-14 divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            No.
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Staff ID
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Date Clock-in
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Time Clock-in
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Location Clock-in
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Time Clock-out
+                        </th>
+                        <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                            Location Clock-out
+                        </th>
+                    </tr>
+                </thead>
 
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            @php
-                            $no = 1
-                            @endphp
-                            @foreach ($history as $row)
-                            <tr>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-sm text-gray-900"> {{ $no++ }} </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-sm text-gray-900"> {{ $row->staff_id }} </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-sm text-gray-900"> {{ $row->date_checkIn }} </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap">
-                                    <div class="text-sm text-gray-900"> {{ $row->time_checkIn }} </div>
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $row->location_checkIn }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                                    {{ $row->time_checkOut }}
-                                </td>
-                                <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"> {{ $row->location_checkOut }} </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+                <tbody class="bg-white divide-y divide-gray-200">
+                    @php
+                    $no = 1
+                    @endphp
+                    @foreach ($history as $row)
+                    <tr>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900"> {{ $no++ }} </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900"> {{ $row->staff_id }} </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900"> {{ $row->date_checkIn }} </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap">
+                            <div class="text-sm text-gray-900"> {{ $row->time_checkIn }} </div>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {{ $row->location_checkIn }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                            {{ $row->time_checkOut }}
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500"> {{ $row->location_checkOut }} </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
     </x-setting2>
 </x-app-layout>
